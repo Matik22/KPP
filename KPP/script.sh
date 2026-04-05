e6f851ff27fc56565b74e8a54f8477132a1bcbb4
Matvey <matveyzhuravlev40@gmail.com>
2026-04-05 21:49:49 +0200

    Фикс скрипта

--- KPP/script.sh
+++ KPP/script.sh
@@ -1,11 +1,11 @@
 cd "$(dirname "$0")"
 g++ -std=c++17 -Wall \
-    main.cpp \
-    User.cpp \
-    Employee.cpp \
-    Client.cpp \
-    Service.cpp \
-    CMenu.cpp \
-    CMenuItem.cpp \
-    -o app
+main.cpp \
+User.cpp \
+Photographer.cpp \
+Administrator.cpp \
+Photo.cpp \
+CMenu.cpp \
+CMenuItem.cpp \
+-o app
 ./app
